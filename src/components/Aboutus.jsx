46b332217fe125205301a10627f6f1aff14e7ca4
phantom-254 <s9763008@gmail.com>
2025-04-22 16:30:16 +0300

    removed image

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -6,7 +6,7 @@ const Aboutus = () => {
   return (
     <div className='col-md-center p-2'>
         <h1><b><i>About Lockheed Auto</i></b></h1>
-        {/* <img src="logo.png" alt="#" /> */}
+        
         <p>Lockheed Auto is a shopping center for vehicles, located in Burnberry, Nostal. We have a large variety of vehicles for our customers to choose from ranging from ; Sport Utility , Sports, J.D.M, family and others.</p><br />
         <p>We try to keep our stock diverse so as to be able to meet the expectations and desires of our customers. For that reason we have vehicles from every major vehicle manufacturer in the world. So be free to explore our collection and be assured to find one that tickles your fancy.</p>
         {/* link to available vehicles */}
